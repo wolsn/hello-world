86b83c1a8ecc7b33780234eca483590feab42e2e
wolsn <32201643+wolsn@users.noreply.github.com>
2017-09-22 18:32:04 +0200

    Create hello-world.py
    Testing GitHubs functionality

---
+++ hello-world.py
@@ -0,0 +1,6 @@
+#Test file
+
+def hello_world(): #Function that returns a phrase
+    phrase = str("Hello World!") #The phrase to be returned
+    return phrase #Returns the phrase and ends the function
+print(hello_world()) #Runs the function and prints the output
